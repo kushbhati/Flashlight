bba8a10a3f17ec85ea4656e83de49eed25b903a8
Kushagra <kushagrabhati7@gmail.com>
2025-04-16 15:06:26 +0530

    added app logo

--- app/src/main/java/exitcodeone/flash/pages/TorchPage.kt
+++ app/src/main/java/exitcodeone/flash/pages/TorchPage.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 
@@ -23,17 +27,27 @@ class TorchPage : Screen {
         Scaffold {
             Column(
                 modifier = Modifier.padding(it).fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Switch(
-                    checked = screenModel.isTorchOn.observeAsState().value == true,
-                    onCheckedChange = {
-                        screenModel.toggleTorch()
-                    },
-                    modifier = Modifier
-                        .rotate(-90f)
-                        .scale(3f)
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Switch(
+                        checked = screenModel.isTorchOn.observeAsState().value == true,
+                        onCheckedChange = {
+                            screenModel.toggleTorch()
+                        },
+                        modifier = Modifier
+                            .rotate(-90f)
+                            .scale(3f)
+                    )
+                }
+                Text(
+                    text = "ex1",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.W700,
+                    modifier = Modifier.padding(16.dp)
                 )
             }
         }
